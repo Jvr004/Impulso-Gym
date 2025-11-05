5b727a7a7d4aac0726653ea3af46f34d1f6db49f
LaloSL <127783873+LaloSL@users.noreply.github.com>
2025-11-04 21:48:03 -0300

    saco boton

--- aset/js/contacto.js
+++ aset/js/contacto.js
@@ -78,13 +78,3 @@ function validar() {
     }
 }
 
-// clase del boton
-const botonInicio = document.querySelector(".button1");
-
-// funcion vuelve al inicio
-function volverInicio() {
-    window.location.href = "index.html";
-}
-
-// escuchar el click y llamar a la funcion
-botonInicio.addEventListener("click", volverInicio);
